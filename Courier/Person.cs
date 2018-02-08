19431ef8738f14e4969bd8dedded0b6f7dc46bf6
Alec Burgdorf <aeburgd@gmail.com>
2018-02-08 12:54:43 -0600

    fix Person class; add Person method; add NukaSunset field

--- Courier/Person.cs
+++ Courier/Person.cs
@@ -15,7 +15,7 @@ namespace Person
         public int Age { get; set; }
         public string[] Skills { get; set; }
         public string[] Traits { get; set; }
-        public string NukaSars { get; set; }
+        public string NukaSunset { get; set; }
         public Dictionary<string, int> Special { get; set; } = new Dictionary<string, int>()
             {
                 { "Strength", 5 },
@@ -48,30 +48,52 @@ namespace Person
         private string[] PickSkills()
         {
             string[] pickedSkills = new string[2];
-            var skills = new List<string>(new string[] {"energy weapons", "melee weapons", "guns", "barter",
+            List<string> skills = new List<string>(new string[] {"energy weapons", "melee weapons", "guns", "barter",
             "repair", "speech", "explosives", "unarmed", "medicine" });
             
 
-            // hard-coded 2 because picked skills always start with 2
-            for (int i = 0; i < 2; i++)
+            // two items in the picked array, always two skills
+            for (int i = 0; i < 1; i++)
             {
                 pickedSkills[i] = skills[rnd.Next(skills.Count)];
             }
             return pickedSkills;
         }
 
+        private string[] TagTraits()
+        {
+            string[] taggedTraits = new string[3];
+            List<string> traits = new List<string>(new string[] { "built to destroy", "fast shot", "four eyes", "good natured", "heavy natured",
+            "kamikaze", "loose cannon", "small frame", "trigger discipline", "wild wasteland" });
+
+            for (int i = 0; i < 2; i++)
+            {
+                taggedTraits[i] = traits[rnd.Next(traits.Count)];
+            }
+            return taggedTraits;
+        }
+
+        private string NukaOrSunset()
+        {
+            string[] sodas = new string[] { "Nuka-Cola", "Sunset Sarsparilla" };
+            return sodas[rnd.Next(0, 1)];
+        }
+
         public Person()
         {
-            var genders = new[] { "male", "female", "agender", "androgyne", "bigender", "nonbinary", "pangender", "transfeminine", "transmasculine"};
+            string[] genders = new string[] { "male", "female", "agender", "androgyne", "bigender", "nonbinary", "pangender", "transfeminine", "transmasculine"};
             Dictionary<string, int> Special = new Dictionary<string, int>();
             Person Courier = new Person();
             Courier.Gender = Courier.PickRandom(genders);
             Courier.NameGender = Courier.ChooseNameGender(Courier);
             Courier.Name = Person.FullName.ToString();
+            Courier.NukaSunset = Courier.NukaOrSunset();
             foreach (KeyValuePair<string, int> attribute in Courier.Special)
             {
                 Courier.Special[attribute.Key] = Courier.Random.Int(1, 10);
             }
+            Courier.Traits = Courier.TagTraits();
+            Courier.Skills = Courier.PickSkills();
         }
     }
 }
